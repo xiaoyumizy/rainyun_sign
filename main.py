726dbf92f1358e8a3d07e3b81ed55f6c19105b88
kndxhz <113306265+kndxhz@users.noreply.github.com>
2024-01-16 01:12:18 +0800

    Update main.py

--- main.py
+++ main.py
@@ -51,4 +51,4 @@ print('==============================')
 if tgpush == 'true':
    sendmessage = f'#雨云自动签到\n签到通知\n用户ID：{ID}\n用户名：{name}\n签到前积分：{pointsbefore}\n当前积分：{points}\n返回值：{res_lqjf_text}'
    bot = telepot.Bot(BOT_TOKEN)
-   bot.sendMessage(CHAT_ID, sendmessage, parse_mode=None, disable_web_page_preview=True, disable_notification=None, reply_to_message_id=None, reply_markup
+   bot.sendMessage(CHAT_ID, sendmessage, parse_mode=None, disable_web_page_preview=True, disable_notification=None, reply_to_message_id=None, reply_markup)
