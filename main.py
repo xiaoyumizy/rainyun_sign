8806e9cf58850a975cfc6b62ca9967c682473130
kndxhz <113306265+kndxhz@users.noreply.github.com>
2024-01-16 03:25:43 +0800

    Update main.py

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
 import requests
 import json
-import telepot
 import os
 
 #Api信息
@@ -9,7 +8,8 @@ tgpush = 'true'
 # 获取私密仓库变量
 api_key = os.environ.get('API_KEY')
 bot_token = os.environ.get('BOT_TOKEN')
-chat_id = os.environ.get('CCHAT_ID')
+chat_id = os.environ.get('CHAT_ID')
+
 
 #请求用户信息
 url = "https://api.v2.rainyun.com/user/"
@@ -48,8 +48,6 @@ print('==============================')
 try:
    #推送
    if tgpush == 'true':
-       sendmessage = '''傻逼雨云自动签到\n用户ID：{0}\n用户名：{1}\n签到前积分：{2}\n当前积分：{3}\n签到结果\n<pre>{4}</pre>'''.format(ID, name, pointsbefore, points,res_lqjf.text)
-       bot = telepot.Bot(bot_token)
-       bot.sendMessage(chat_id, sendmessage, parse_mode='HTML', disable_web_page_preview=True, disable_notification=None, reply_to_message_id=None, reply_markup=None)
+       a = requests.request("GET", f'https://api.telegram.org/bot{bot_token}/sendmessage?chat_id={chat_id}&text=傻逼雨云自动签到\n用户ID：{ID}\n用户名：{name}\n签到前积分：{pointsbefore}\n当前积分：{points}\n签到结果\n<pre>{res_lqjf.text}</pre>&parse_mode=HTML') 
 except Exception as e:
     print(f"发生了一个错误: {e}")
