18af8190a36ba03e9e9e0ee98d4111c77700595e
kndxhz <113306265+kndxhz@users.noreply.github.com>
2024-01-16 01:23:26 +0800

    Update main.py

--- main.py
+++ main.py
@@ -17,38 +17,46 @@ payload = {}
 headers_yh = {
    'X-Api-Key': API_KEY
 }
-res_points = requests.request("GET", url, headers=headers_yh, data=payload)
-zh_json = res_points.json()
-pointsbefore = zh_json['data']['Points']
-ID = zh_json['data']['ID']
-name = zh_json['data']['Name']
-print(f'ID：{ID}')
-print(f'用户名：{name}')
-print(f'剩余积分：{pointsbefore}')
-print('==============================')
+try:
+    res_points = requests.get(url, headers=headers_yh, data=payload)
+    res_points.raise_for_status()  # 检查HTTP错误
+    zh_json = res_points.json()
+    pointsbefore = zh_json['data']['Points']
+    ID = zh_json['data']['ID']
+    name = zh_json['data']['Name']
+    print(f'ID：{ID}')
+    print(f'用户名：{name}')
+    print(f'剩余积分：{pointsbefore}')
+    print('==============================')
 
-# 签到部分
-url_lqjf = 'https://api.v2.rainyun.com/user/reward/tasks'
-headers_lqjf = {
-    'content-type': "application/json",
-    'X-Api-Key': API_KEY
-}
-body_lqjf = {
-    "task_name": '每日签到',
-    "verifyCode": ''
-}
-res_lqjf = requests.request("POST", url_lqjf, headers=headers_lqjf, data=json.dumps(body_lqjf))
-res_points = requests.request("GET", url, headers=headers_yh, data=payload)
-zh_json = res_points.json()
-points = zh_json['data']['Points']
-if points == pointsbefore + 300:
-    print(f'签到成功，当前剩余积分：{points + 300}')
-else:
-    print(f'签到失败，返回值：{res_lqjf.text}')
-print('==============================')
+    # 签到部分
+    url_lqjf = 'https://api.v2.rainyun.com/user/reward/tasks'
+    headers_lqjf = {
+        'content-type': 'application/json',
+        'X-Api-Key': API_KEY
+    }
+    body_lqjf = {
+        'task_name': '每日签到',
+        'verifyCode': ''
+    }
+    res_lqjf = requests.post(url_lqjf, headers=headers_lqjf, data=json.dumps(body_lqjf))
+    res_lqjf.raise_for_status()  # 检查HTTP错误
+    res_points = requests.get(url, headers=headers_yh, data=payload)
+    zh_json = res_points.json()
+    points = zh_json['data']['Points']
+    if points == pointsbefore + 300:
+        print(f'签到成功，当前剩余积分：{points + 300}')
+    else:
+        print(f'签到失败，返回值：{res_lqjf.text}')
+    print('==============================')
 
-# 推送
-if tgpush == 'true':
-   sendmessage = f'#雨云自动签到\n签到通知\n用户ID：{ID}\n用户名：{name}\n签到前积分：{pointsbefore}\n当前积分：{points}\n返回值：{res_lqjf.text}'
-   bot = telepot.Bot(BOT_TOKEN)
-   bot.sendMessage(CHAT_ID, sendmessage)
+    # 推送
+    if tgpush == 'true':
+        sendmessage = f'#雨云自动签到\n签到通知\n用户ID：{ID}\n用户名：{name}\n签到前积分：{pointsbefore}\n当前积分：{points}\n返回值：{res_lqjf.text}'
+        bot = telepot.Bot(BOT_TOKEN)
+        bot.sendMessage(CHAT_ID, sendmessage)
+
+except requests.RequestException as e:
+    print(f"请求过程中发生错误: {e}")
+except Exception as ex:
+    print(f"发生了意外错误: {ex}")
