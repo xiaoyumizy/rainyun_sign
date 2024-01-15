50c36f1b566c22ae368ca09440e3f906707289ff
kndxhz <113306265+kndxhz@users.noreply.github.com>
2024-01-16 00:26:06 +0800

    Update main.py

--- main.py
+++ main.py
@@ -1,15 +1,19 @@
+import os
 import requests
 import json
 import telepot
 
-#Api信息
-
+# Api信息
 tgpush = 'true'
 
+# 获取私密仓库变量
+API_KEY = os.environ.get('API_KEY')
+BOT_TOKEN = os.environ.get('BOT_TOKEN')
+MSG_ID = os.environ.get('MSG_ID')
 
-#请求用户信息
+# 请求用户信息
 url = "https://api.v2.rainyun.com/user/"
-payload={}
+payload = {}
 headers_yh = {
    'X-Api-Key': API_KEY
 }
@@ -22,28 +26,29 @@ print(f'ID：{ID}')
 print(f'用户名：{name}')
 print(f'剩余积分：{pointsbefore}')
 print('==============================')
-#签到部分
+
+# 签到部分
 url_lqjf = 'https://api.v2.rainyun.com/user/reward/tasks'
 headers_lqjf = {
-    'content-type':"application/json",
-    'X-Api-Key':API_KEY
-    }
+    'content-type': "application/json",
+    'X-Api-Key': API_KEY
+}
 body_lqjf = {
-    "task_name" : '每日签到',
-    "verifyCode" : ''
-    }
-res_lqjf = requests.request("POST", url_lqjf, headers=headers_lqjf, data = json.dumps(body_lqjf))
+    "task_name": '每日签到',
+    "verifyCode": ''
+}
+res_lqjf = requests.request("POST", url_lqjf, headers=headers_lqjf, data=json.dumps(body_lqjf))
 res_points = requests.request("GET", url, headers=headers_yh, data=payload)
 zh_json = res_points.json()
 points = zh_json['data']['Points']
-if points == {pointsbefore + 300}:
+if points == pointsbefore + 300:
     print(f'签到成功，当前剩余积分：{points + 300}')
 else:
     print(f'签到失败，返回值：{res_lqjf.text}')
 print('==============================')
-#推送
+
+# 推送
 if tgpush == 'true':
     sendmessage = '''雨云自动签到Bot\n签到通知\n用户ID：{0}\n用户名：{1}\n签到前积分：{2}\n当前积分：{3}\nhttps://github.com/ZYGLQexplorer/RainYun-Checkin'''.format(ID, name, pointsbefore, points)
     bot = telepot.Bot(BOT_TOKEN)
     bot.sendMessage(MSG_ID, sendmessage, parse_mode=None, disable_web_page_preview=True, disable_notification=None, reply_to_message_id=None, reply_markup=None)
-
