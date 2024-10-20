681d9dd4c4d1623dd90924cb1262bb81815002dc
可耐的小伙纸 <kndxhz@163.com>
2024-10-20 12:47:50 +0800

    Update main.py

--- main.py
+++ main.py
@@ -47,5 +47,5 @@ print('==============================')
 if tgpush == 'true': # 推送
   sendmessage = '''#傻逼雨云自动签到\n用户ID：{0}\n用户名：{1}\n签到前积分：{2}\n当前积分：{3}\n积分价值：{5} CNY\n签到结果\n<pre>{4}</pre>'''.format(ID, name, pointsbefore, points,res_lqjf.text, round(int(points)/2000))
   bot = telepot.Bot(bot_token)
-  bot.sendMessage(msgid, sendmessage, parse_mode='HTML', disable_web_page_preview=True, disable_notification=None, reply_to_message_id=None, reply_markup=None)
+  bot.sendMessage(chat_id, sendmessage, parse_mode='HTML', disable_web_page_preview=True, disable_notification=None, reply_to_message_id=None, reply_markup=None)
 
